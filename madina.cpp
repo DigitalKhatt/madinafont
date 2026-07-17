40dd2d4c4f703ce4abeecef0be81ca3f5f095e8b
Amine Anane <ananeamine@gmail.com>
2026-07-17 12:32:59 -0400

    Update font project for stdlib layout API

--- madina.cpp
+++ madina.cpp
@@ -2131,7 +2131,8 @@ Lookup* Madina::forsmallhighwaw() {
   lookup->name = "forsmallhighwaw";
   lookup->feature = "rlig";
   lookup->type = Lookup::chainingsub;
-  lookup->markGlyphSetIndex = m_layout->addMarkSet(QList{(std::uint16_t)glyphs["smallhighwaw"].charcode});
+  lookup->markGlyphSetIndex = m_layout->addMarkSet(
+      std::vector<std::uint16_t>{(std::uint16_t)glyphs["smallhighwaw"].charcode});
   lookup->flags = lookup->flags | Lookup::Flags::UseMarkFilteringSet;
 
   // forsmallalefwithmaddah
